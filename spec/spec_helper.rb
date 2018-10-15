b5f972ab369ffdc43051548321c783d6f0a8de3b
sue445 <sue445@sue445.net>
2018-10-15 21:52:59 +0900

    Setup coveralls, codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+if ENV["CI"]
+  require "simplecov"
+  require "coveralls"
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    %w[spec].each do |ignore_path|
+      add_filter(ignore_path)
+    end
+  end
+end
+
 require "bundler/setup"
 require "pixela"
 require "webmock/rspec"
