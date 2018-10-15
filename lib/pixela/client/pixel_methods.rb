60be67ba5ae17597bfa6ec356141ff6843b03994
sue445 <sue445@sue445.net>
2018-10-15 21:27:54 +0900

    Impl update_pixel

--- lib/pixela/client/pixel_methods.rb
+++ lib/pixela/client/pixel_methods.rb
@@ -32,4 +32,23 @@ module Pixela::Client::PixelMethods
       connection.get("users/#{username}/graphs/#{graph_id}/#{to_ymd(date)}", nil, user_token_headers).body
     end
   end
+
+  # Update the quantity already registered as a "Pixel".
+  #
+  # @param graph_id [String]
+  # @param date     [Date,Time]
+  # @param quantity [Integer,Float]
+  #
+  # @return [Hashie::Mash]
+  #
+  # @see https://pixe.la/#api-pixel
+  def update_pixel(graph_id:, date:, quantity:)
+    params = {
+      quantity: quantity.to_s,
+    }
+
+    with_error_handling do
+      connection.put("users/#{username}/graphs/#{graph_id}/#{to_ymd(date)}", params, user_token_headers).body
+    end
+  end
 end
--- spec/pixela/client/pixel_methods_spec.rb
+++ spec/pixela/client/pixel_methods_spec.rb
@@ -47,4 +47,31 @@ RSpec.describe Pixela::Client::PixelMethods do
 
     its(:quantity) { should eq 5 }
   end
+
+  describe "#update_pixel" do
+    subject do
+      client.update_pixel(
+        graph_id: graph_id,
+        date:     date,
+        quantity: quantity,
+      )
+    end
+
+    let(:graph_id) { "test-graph" }
+    let(:date)     { Date.parse("2018-09-15") }
+    let(:quantity) { 7 }
+
+    before do
+      json_body = <<~JSON.strip
+        {"quantity":"7"}
+      JSON
+
+      stub_request(:put, "https://pixe.la/v1/users/a-know/graphs/test-graph/20180915").
+        with(body: json_body, headers: user_token_headers).
+        to_return(status: 200, body: fixture("success.json"))
+    end
+
+    its(:message)   { should eq "Success." }
+    its(:isSuccess) { should eq true }
+  end
 end
